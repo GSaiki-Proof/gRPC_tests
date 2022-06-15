46b9a58d635e6afa8ee0651273421a573e2723ce
Saiki <gustavo.saiki@proof.com.br>
2022-06-15 11:49:51 -0300

    Fixed: Server running

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,23 +1,32 @@
 // Libs
-import grpc from '@grpc/grpc-js';
+import * as grpc from '@grpc/grpc-js';
+import * as protoLoader from '@grpc/proto-loader';
 
 // Data
 const HOST = '0.0.0.0:3000';
-const PROTO_FILE = './proto/hello.proto';
+const PROTO_FILE = './src/protos/character.proto';
 
 const server = new grpc.Server();
-const proto: any = grpc.load(PROTO_FILE, 'hello', '1.0.0');
 
-// Define the routes
-server.addService(proto.hello.HelloService.service, {
-    sayHello: (call: any, callback: any) => {
-        callback(null, { message: 'Hello, ' + call.request.name });
-    }
-});
 
-// Start the Server
-server.bindAsync(HOST, grpc.ServerCredentials.createInsecure(),
-    (err: Error | null, port: number) => {
-        console.log(`The server is online on port: ${port}`);
-    }
-);
+// Functions
+async function main() {
+    const proto = protoLoader.loadSync(PROTO_FILE);
+    const characterProto: any = grpc.loadPackageDefinition(proto).character;
+
+    // Define the routes
+    server.addService(characterProto.CharacterService.service, {
+        sayHello: (call: any, callback: any) => {
+            callback(null, { message: 'Hello, ' + call.request.name });
+        }
+    });
+
+    // Start the Server
+    server.bindAsync(HOST, grpc.ServerCredentials.createInsecure(),
+        (err, port) => {
+            console.log(`The server is online on port: ${port}`);
+        }
+    );
+}
+
+main();
